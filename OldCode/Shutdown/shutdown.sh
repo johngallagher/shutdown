7f29f6cabe2eb338775ccba69488422168ee0906
John Gallagher <john@synapticmishap.co.uk>
2014-07-31 20:30:59 +0100

    Removed old code.

--- OldCode/Shutdown/shutdown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# shutdown.sh - this currently needs to be manually copied to /opt/local/sbin
-# Shutdown
-#
-# Created by John Gallagher on 19/11/2009.
-# Copyright 2009 Synaptic Mishap. All rights reserved.
-
-shutdown -h +1
