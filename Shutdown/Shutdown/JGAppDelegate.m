d675fe5b88ce5ead6893d08a7b58cf915da41e59
John Gallagher <john@synapticmishap.co.uk>
2014-08-22 07:56:18 +0100

    Tidied up app delegate refactoring.

--- Shutdown/Shutdown/JGAppDelegate.m
+++ Shutdown/Shutdown/JGAppDelegate.m
@@ -1,19 +1,64 @@
 #import "JGAppDelegate.h"
 #import "JGSystemDisabler.h"
 
+@interface JGAppDelegate ()
++(void)registerDefaults;
+
+-(void)watchForWakeFromSleep;
+
+-(void)watchForChangesInStartupShutdownTime;
+
+-(void)addStatusMenuBar;
+
+-(void)refreshSystemStateStartupDefaults:(NSUserDefaults *)defaults;
+
+-(BOOL)startup:(NSDate *)startup isBeforeShutdown:(NSDate *)shutdown;
+@end
+
 @implementation JGAppDelegate
 
 +(void)initialize {
   [super initialize];
-  NSDateFormatter *format = [[NSDateFormatter alloc] init];
-  [format setTimeStyle:NSDateFormatterShortStyle];
-  [format setDateStyle:NSDateFormatterNoStyle];
-
-  [[NSUserDefaults standardUserDefaults] registerDefaults:@{@"startup": [format dateFromString:@"7:00"], @"shutdown": [format dateFromString:@"21:00"]}];
+  [self registerDefaults];
   [NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory];
 }
 
 -(void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  [self watchForChangesInStartupShutdownTime];
+  [self addStatusMenuBar];
+  [self watchForWakeFromSleep];
+}
+
+-(IBAction)showPreferencesWindow:(id)sender {
+  [NSApp activateIgnoringOtherApps:YES];
+  [[self window] makeKeyAndOrderFront:self];
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary *)change
+                      context:(void *)context {
+  [self refreshSystemStateStartupDefaults:object];
+}
+
+#pragma mark private methods
+
++(void)registerDefaults {
+  NSDateFormatter *format = [[NSDateFormatter alloc] init];
+  [format setTimeStyle:NSDateFormatterShortStyle];
+  [format setDateStyle:NSDateFormatterNoStyle];
+
+  [[NSUserDefaults standardUserDefaults] registerDefaults:@{@"startup": [format dateFromString:@"7:45"], @"shutdown": [format dateFromString:@"21:00"]}];
+}
+
+-(void)watchForWakeFromSleep {
+  [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+                                                         selector:@selector(refreshSystemStateStartupDefaults:)
+                                                             name:NSWorkspaceDidWakeNotification
+                                                           object:[NSUserDefaults standardUserDefaults]];
+}
+
+-(void)watchForChangesInStartupShutdownTime {
   [[NSUserDefaults standardUserDefaults] addObserver:self
                                           forKeyPath:@"startup"
                                              options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionInitial)
@@ -22,6 +67,10 @@
                                           forKeyPath:@"shutdown"
                                              options:NSKeyValueObservingOptionNew
                                              context:NULL];
+}
+
+
+-(void)addStatusMenuBar {
   statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
   [statusItem setMenu:menu];
   [statusItem setAlternateImage:[NSImage imageNamed:NSImageNameLockLockedTemplate]];
@@ -29,19 +78,9 @@
   [statusItem setHighlightMode:YES];
 }
 
--(IBAction)showPreferencesWindow:(id)sender {
-  [NSApp activateIgnoringOtherApps:YES];
-  [[self window] makeKeyAndOrderFront:self];
-}
-
-
--(void)observeValueForKeyPath:(NSString *)keyPath
-                     ofObject:(id)object
-                       change:(NSDictionary *)change
-                      context:(void *)context {
-  NSDate *startup = [object valueForKey:@"startup"];
-  NSDate *shutdown = [object valueForKey:@"shutdown"];
-
+-(void)refreshSystemStateStartupDefaults:(NSUserDefaults *)defaults {
+  NSDate *startup = [defaults valueForKey:@"startup"];
+  NSDate *shutdown = [defaults valueForKey:@"shutdown"];
   if([self startup:startup isBeforeShutdown:shutdown]) {
     NSLog(@"Startup and shutdown times are good...");
     state = [JGSystemDisabler disablerWithStartupTime:startup shutdownTime:shutdown disablerView:blockerView];
@@ -51,7 +90,7 @@
 }
 
 -(BOOL)startup:(NSDate *)startup isBeforeShutdown:(NSDate *)shutdown {
-  return [startup compare:shutdown] != NSOrderedDescending;
+  return [startup compare:shutdown] == NSOrderedAscending;
 }
 
 @end
